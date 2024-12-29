938ed9532702328ecef3832213f0247e018bec7a
ArielBaron <arielbar80@gmail.com>
2024-12-29 19:24:40 +0200

    Added BetterMashovLive parts
    by tranforming The api to just a func and make active thus when collectin data at "submit" api point

--- backend/Server.js
+++ backend/Server.js
@@ -3,79 +3,137 @@ import bodyParser from 'body-parser';;
 // Funcs for fetching mashov data
 import fetchBehavior from './mashov/behavior.js';
 import fetchGrades  from './mashov/grades.js'
-import getTimetableAndChanges from './getTimeTable.js';
+import fetchTimetable from './mashov/timetable.js';
+import getIsCoolTimetableAndChanges from './getTimeTable.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-function encodeHebClass(hebClassStr) {
-  const hebrewToEnglishMap = {
-    'א': 'a',
-    'ב': 'b',
-    'ג': 'c',
-    'ד': 'd',
-    'ה': 'e',
-    'ו': 'f',
-    'ז': 'g',
-    'ח': 'h',
-    'ט': 'i',
-    'י': 'j',
-    'י"א': 'k',
-    'י"ב': 'l'
-  };
+const getMashovTimetable = async(loginInfo) => {
 
-  // Match the class prefix and replace using the map
-  return hebClassStr
-    .replace(/י"ב|י"א|[א-י]/g, match => hebrewToEnglishMap[match] || match) // Map Hebrew to English
-    .replace(/^([a-z])(\d)/i, '$1-$2'); // Ensure there's a '-' between the letter and number
-}
-const getGrades = async (loginInfo) => {
-    if (!loginInfo) {
-      throw new Error('Not logged in to Mashov');
-    }
-  
-    const grades = await fetchGrades(loginInfo);
-    // Process the grades data
-    const processedGrades = grades.map(obj => {
-      delete obj.groupId;
-      delete obj.gradingPeriod;
-      delete obj.gradeTypeId;
-      delete obj.gradeRate;
-      delete obj.id;
-      delete obj.gradingEventId;
-      delete obj.studentGuid;
-      delete obj.rate;
-      delete obj.year;
+
+    const timetable = await fetchTimetable(loginInfo);
+
+    // Process the timetable data
+    const processedTimetable = timetable.map(obj => {
+      delete obj.groupDetails.groupInactiveTeachers;
+      delete obj.groupDetails.groupId;
+      delete obj.groupDetails.subjectName;
+
+      const { day, lesson } = obj.timeTable;
+      obj.groupDetails.day = day;
+      obj.groupDetails.lesson = lesson;
+      delete obj.timeTable;
+
+      const teacherNames = obj.groupDetails.groupTeachers.map(teacher => teacher.teacherName);
+      obj.groupDetails.teacherName = teacherNames.join(", ");
+      delete obj.groupDetails.groupTeachers;
+
       return obj;
     });
-  
-    // Structure grades by subject
-    const subjectGrades = {};
-    processedGrades.forEach(entry => {
-      const { subjectName, grade } = entry;
-  
-      if (subjectGrades[subjectName]) {
-        subjectGrades[subjectName].push(grade);
-      } else {
-        subjectGrades[subjectName] = [grade];
-      }
-    });
-  
-    return { subjectGrades, gradesData: processedGrades };
-  };
 
-  app.get('/submit', async (req, res) => {
-    const { id, semel,  userClass, password } = req.query;  // Change req.body to req.query
-    const loginInfo =  {
-      "SEMEL": semel,
-      "ID": id,
-      "PASSWORD": password,
-      "CLASS": userClass 
+    // Split timetableData into sublists based on the day property
+    const timetableByDay = {};
+    processedTimetable.forEach(obj => {
+      const { day, ...rest } = obj.groupDetails;
+      if (!timetableByDay[day]) {
+        timetableByDay[day] = [];
+      }
+      timetableByDay[day].push(rest);
+    });
+
+    // Sort by lesson
+    for (const day in timetableByDay) {
+      timetableByDay[day].sort((a, b) => a.lesson - b.lesson);
     }
-    console.log(await getTimetableAndChanges(loginInfo));
-    res.status(200).json({ message: 'Form submitted successfully', data: req.query });  // Use req.query here as well
+
+    return timetableByDay;
+};
+
+// Process and filter behavior data
+const getBehavior = async(loginInfo) => {
+
+  const behavior = await fetchBehavior(loginInfo);
+
+  // Process the behavior data
+  const processedBehavior = behavior.map(obj => {
+    if (obj.achvaName !== "נוכחות בשיעור מקוון") {
+      delete obj.timestamp;
+    }
+    delete obj.reporterGuid;
+    delete obj.lessonId;
+    delete obj.achvaCode;
+    delete obj.achvaAval;
+    delete obj.justificationId;
+    delete obj.studentGuid;
+    delete obj.eventCode;
+    delete obj.lessonType;
+    delete obj.lessonReporter;
+    obj.justified = obj.justified === 9 ? "מוצדק" : "לא מוצדק";
+    delete obj.groupId;
+
+    // Format lesson date
+    const lessonDate = new Date(obj.lessonDate).toISOString().split('T')[0];
+    obj.lessonDate = lessonDate.split("-").reverse().join("-");
+    return obj;
+  });
+
+  return processedBehavior
+}
+
+const getGrades = async (loginInfo) => {
+  if (!loginInfo) {
+    throw new Error('Not logged in to Mashov');
+  }
+
+  const grades = await fetchGrades(loginInfo);
+  // Process the grades data
+  const processedGrades = grades.map(obj => {
+    delete obj.groupId;
+    delete obj.gradingPeriod;
+    delete obj.gradeTypeId;
+    delete obj.gradeRate;
+    delete obj.id;
+    delete obj.gradingEventId;
+    delete obj.studentGuid;
+    delete obj.rate;
+    delete obj.year;
+    return obj;
+  });
+
+  // Structure grades by subject
+  const subjectGrades = {};
+  processedGrades.forEach(entry => {
+    const { subjectName, grade } = entry;
+
+    if (subjectGrades[subjectName]) {
+      subjectGrades[subjectName].push(grade);
+    } else {
+      subjectGrades[subjectName] = [grade];
+    }
+  });
+
+  return { subjectGrades, gradesData: processedGrades };
+};
+
+
+app.get('/submit', async (req, res) => {
+  const { id, semel,  userClass, password } = req.query;  // Change req.body to req.query
+  const loginInfo =  {
+    "SEMEL": semel,
+    "ID": id,
+    "PASSWORD": password,
+    "CLASS": userClass,
+    "YEAR": new Date().getFullYear() + 1
+  }
+  const gradesData = await getGrades(loginInfo);
+  const behaviorData = await getBehavior(loginInfo);
+  const iscoolTimetableAndChangesData = await getIsCoolTimetableAndChanges(loginInfo);
+  const mashovTimetableData = await getMashovTimetable(loginInfo);
+  console.log(mashovTimetableData)
+  res.status(200).json({ message: 'Form submitted successfully', data: req.query });  // Use req.query here as well
 });
 
 app.listen(PORT, () => {
--- backend/mashov/timetable.js
+++ backend/mashov/timetable.js
@@ -1,7 +1,12 @@
 import mashov from 'mashov-api';
 import fs from 'fs';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const fetchTimetable = async (credentials) => {
+    
     const { SEMEL, YEAR, ID, PASSWORD } = credentials;
     try {
         const loginInfo = await mashov.loginToMashov(SEMEL, YEAR, ID, PASSWORD);
@@ -12,5 +17,11 @@ const fetchTimetable = async (credentials) => {
         throw error;
     }
 };
+// const loginInfo =  {
+//     "ID": process.env.REACT_APP_EX_ID,
+//     "PASSWORD": process.env.REACT_APP_EX_PASSWORD,
+//     "SEMEL": process.env.REACT_APP_EX_SEMEL,
+//     "YEAR": new Date().getFullYear()+1,
+// }
 
 export default fetchTimetable;
