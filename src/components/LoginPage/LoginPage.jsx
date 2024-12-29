016bb5e1bd8ea8627381367bd351401a3e4e6276
ArielBaron <arielbar80@gmail.com>
2024-12-29 13:53:02 +0200

    added auto filling file input for easier use

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,12 +2,30 @@ import "./LoginPage.css"
 function LoginPage() {
     const backendPort = process.env.REACT_APP_PORT;
     const submitApi = `http://localhost:${backendPort}/submit`;
-    
+    const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const inputs = JSON.parse(e.target.result)[0];
+          console.log(inputs);
+          for(const inputName of Object.keys(inputs)){
+            console.log(inputName);
+          }
+          document.getElementById("id-inp").value = inputs["ID"];
+          document.getElementById("semel-inp").value = inputs["SEMEL"];
+          document.getElementById("password-inp").value = inputs["PASSWORD"];
+          document.getElementById("class-inp").value = inputs["CLASS"]
+        };
+        reader.readAsText(file); // Adjust based on file type
+      } else {
+        console.log("No file selected");
+      }
+    };
     return (
       <>
         <img src="EduSphereLogo.png" alt="" />
-        <label htmlFor="auto-complete-inp">Enter a .txt or .json file for auto complete</label>
-        <input type="file" name="AutoCompleteFile" id="auto-complete-inp" accept=".txt, .json "/>
+        <input type="file" name="AutoCompleteFile" onChange={handleFileChange} id="auto-complete-inp" accept=".txt, .json" ></input>
         <form action={`http://localhost:3000/submit`} method="GET">
           <label htmlFor="id-inp">Please Provide your ID Number.</label>
           <input type="text" name="id" id="id-inp" />
